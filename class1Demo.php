36d24c78b062b117a81f448d48530cb6e3520121
Poole <ztpoole@labs.neit.edu>
2018-10-01 13:10:14 -0400

    plz commit

---
+++ class1Demo.php
@@ -0,0 +1,11 @@
+<?php
+
+
+    echo "Erik is my Favorite Professor!";
+/*  
+ *  
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
